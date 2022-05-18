5527a3b074dde43d79a938e036da32172a11bc47
Ian Ross <ianrossdrums@gmail.com>
2022-05-17 18:24:00 -0600

    feat: add merchant and item serializer

---
+++ app/serializers/merchant_serializer.rb
@@ -0,0 +1,27 @@
+class MerchantSerializer
+  def self.format_merchants(merchants)
+    {
+      data: merchants.map do |merchant|
+        {
+          id: merchant.id,
+          attributes:
+          {
+            name: merchant.name
+          }
+        }
+      end
+    }
+  end
+
+  def self.format_merchant(merchant)
+    {
+      data: {
+        id: merchant.id.to_s,
+        type: "merchant",
+        attributes: {
+          name: merchant.name
+        }
+      }
+    }
+  end
+end
