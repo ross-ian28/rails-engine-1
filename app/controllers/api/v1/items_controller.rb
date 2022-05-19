205c5134816306ce5eb3d3998c92bea380492e39
Ian Ross <ianrossdrums@gmail.com>
2022-05-18 18:12:07 -0600

    feat: add item endpoint controllers

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,39 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    merchant = Merchant.find(params[:merchant_id])
-    items = merchant.items
+    items = Item.all
     render json: ItemSerializer.format_items(items)
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: ItemSerializer.format_item(item)
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: ItemSerializer.format_item(item), status: :created
+    else
+      flash[:alert] = "Try again"
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      render json: ItemSerializer.format_item(item)
+    else
+      flash[:alert] = "Try again"
+    end
+  end
+
+  def destroy
+    Item.find(params[:id]).delete
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
   end
 end
