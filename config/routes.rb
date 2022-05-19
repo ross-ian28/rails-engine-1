4648c026f3d62fb4b0fec0e654c3bf35a5fb2094
Ian Ross <ianrossdrums@gmail.com>
2022-05-18 18:11:39 -0600

    feat: add item routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index]
+        resources :items, controller: "merchant_items", only: [:index]
+      end
+      resources :items do
+        resources :merchant, controller: "item_merchant", only: [:index]
       end
     end
   end
