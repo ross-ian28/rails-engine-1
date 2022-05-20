1229bf0b8bbd05cdc6ba51e50e79545b4d36ab8d
Ian Ross <ianrossdrums@gmail.com>
2022-05-20 08:32:33 -0600

    refactor: item now belongs to a merchant

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,3 @@
 class Item < ApplicationRecord
-  has_many :merchants
+  belongs_to :merchant
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,9 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.find_merch(search_name)
+    where("name LIKE ?", search_name)
+      .order(name)
+      .first
+  end
 end
