6a80b016e2deb8d4706e8c3df5ab8e5a90eb537f
Ian Ross <ianrossdrums@gmail.com>
2022-05-20 08:32:13 -0600

    feat: add search routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get "/merchants/find", to: "search#find_merchant"
+      get "/items/find_all", to: "search#find_items"
       resources :merchants, only: [:index, :show] do
         resources :items, controller: "merchant_items", only: [:index]
       end
